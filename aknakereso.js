36fa901c90c1f31240beaa91f79bc3a5815bfc4b
takzol94 <138156599+takzol94@users.noreply.github.com>
2023-07-01 12:35:19 +0200

    canvas-keszites

--- aknakereso.js
+++ aknakereso.js
@@ -1,1 +1,4 @@
-console.log('aknakereso a legkirályabb játék')
+var image = document.getElementById("hidden");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.drawImage(image, 0, 0);
