bd02a125403f33809c0f62b0651a9d2e9004e293
takzol94 <138156599+takzol94@users.noreply.github.com>
2023-07-04 20:06:00 +0200

    tombok

--- aknakereso.js
+++ aknakereso.js
@@ -6,6 +6,13 @@ const size = 50;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
 
+let firstRow = [0, 0, 0, 3, 0, 0, 0, 0, 0, 0];
+let secondRow = [0, 0, 0, 0, 2, 0, 0, 1, 0, 0];
+
+let map = [firstRow, secondRow];
+
+console.log(map);
+
 drawMap();    // Ezzel a függvénnyel rajzoljuk ki a pályát.
 
 function drawMap() {
@@ -13,7 +20,7 @@ function drawMap() {
     for (let i = 0; i < columns; i++) {
         drawImage(i * size, j * size);
     }
-    }
+  }
 }
 
 function drawImage(x, y) {
