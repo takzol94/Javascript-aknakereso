379ee6586824a44116e2f4ae9865c052ccfad590
takzol94 <138156599+takzol94@users.noreply.github.com>
2023-07-05 21:27:41 +0200

    uresmezoaknakkal

--- aknakereso.js
+++ aknakereso.js
@@ -1,4 +1,3 @@
-const image = document.getElementById("hidden");
 const canvas = document.getElementById("myCanvas");
 const c = canvas.getContext("2d");
 
@@ -7,14 +6,27 @@ const columns = canvas.width / size;
 const rows = canvas.height / size;
 const mine = "mine";
 const mineCount = 20;
+const images = {
+  "hidden": document.getElementById("hidden"),
+  "mine": document.getElementById("mine"),
+  "0": document.getElementById("field-0"),
+  "1": document.getElementById("field-1"),
+  "2": document.getElementById("field-2"),
+  "3": document.getElementById("field-3"),
+  "4": document.getElementById("field-4"),
+  "5": document.getElementById("field-5"),
+  "6": document.getElementById("field-6"),
+  "7": document.getElementById("field-7"),
+  "8": document.getElementById("field-8"),
+}
 
 let map = createMap(); // Ezzel a függvénnyel töltjük fel a pályát.
 
 placeMines(map , mineCount)  // Ezzel a függvénnyel helyezzük el random az aknákat a pályán.
 
-console.log(map);
-
 drawMap();    // Ezzel a függvénnyel rajzoljuk ki a pályát.
+
+console.log(map);
 
 function placeMines(map , mineCount) {
   let mines = 0;
@@ -27,7 +39,6 @@ function placeMines(map , mineCount) {
     }
   }
 }
-
 
 function createMap() {
   let map = [];
@@ -44,11 +55,13 @@ function createMap() {
 function drawMap() {
   for (let j = 0; j < rows; j++) {
     for (let i = 0; i < columns; i++) {
-        drawImage(i * size, j * size);
+      let field = map[j][i];
+      let image = images[field]
+      drawImage(image, i * size, j * size);
     }
   }
 }
 
-function drawImage(x, y) {
+function drawImage(image, x,  y) {
   c.drawImage(image, x, y, size, size);
 }
