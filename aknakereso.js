495f0527f8dee54860ec8fc7dbc5004fc61a82ac
takzol94 <138156599+takzol94@users.noreply.github.com>
2023-07-05 20:34:27 +0200

    aknaelhelyezes

--- aknakereso.js
+++ aknakereso.js
@@ -6,14 +6,27 @@ const size = 50;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
 const mine = "mine";
+const mineCount = 20;
 
 let map = createMap(); // Ezzel a függvénnyel töltjük fel a pályát.
 
-map[3][4] = mine;
+placeMines(map , mineCount)  // Ezzel a függvénnyel helyezzük el random az aknákat a pályán.
 
 console.log(map);
 
 drawMap();    // Ezzel a függvénnyel rajzoljuk ki a pályát.
+
+function placeMines(map , mineCount) {
+  let mines = 0;
+  while (mines < mineCount) {
+    let x = Math.floor(Math.random() * columns);
+    let y = Math.floor(Math.random() * rows);
+    if (map[y][x] !== mine) {
+      map[y][x] = mine;
+      mines++;
+    }
+  }
+}
 
 
 function createMap() {
